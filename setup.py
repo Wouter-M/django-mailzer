44a3da65436045d1da30af25d1065545dc7675a9
WouterM <woutermenninga@gmail.com>
2016-05-21 00:34:18 +0200

    Removing long_description reading from file: not found during pip install

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -14,7 +11,7 @@ setup(
     include_package_data=True,
     license='BSD License',  # example license
     description='A django app with mail templating and queueing functionality.',
-    long_description=README,
+    long_description='A django app with mail templating and queueing functionality.',
     url='',
     author='Wouter Menninga',
     author_email='woutermenninga@gmail.com',
